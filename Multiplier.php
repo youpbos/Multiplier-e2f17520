0d2b2c4b26b775df108684d9cf1a49e29b95dd3d
youp bos <2107418@talnet.nl>
2020-01-14 14:49:29 +0100

    Uitwerking van de exercise

---
+++ Multiplier.php
@@ -0,0 +1,8 @@
+<?php
+echo ("ik ga twee getallen met elkaar vermenigvuldigen") . PHP_EOL;
+echo ("wat word het eerste getal?") . PHP_EOL;
+$a = readline("") ;
+echo ("en het tweede getal?") . PHP_EOL;
+$b = readline("") ;
+echo ("antwoord = ") . $a * $b . PHP_EOL;
+?>
